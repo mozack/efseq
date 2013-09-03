d9b747fbcfbfe843d99756f25b63327af67f0580
Lisle Mose <lislemose@gmail.com>
2013-09-03 15:06:46 -0400

    Allow DCS to be run from jar main.

--- src/main/java/efseq/EfSeq.java
+++ src/main/java/efseq/EfSeq.java
@@ -7,7 +7,7 @@ public class EfSeq {
 	public static void run(String[] args) throws IOException {
 		if (args.length < 1) {
 			usage();
-		} else if (args[0].equals("prep")) {
+		} else if (args[0].equals("prep") && args.length == 6) {
 			String fastq1 = args[1];
 			String fastq2 = args[2];
 			String out1 = args[3];
@@ -16,6 +16,15 @@ public class EfSeq {
 			
 			FastqPreprocessor fp = new FastqPreprocessor();
 			fp.preProcess(fastq1, fastq2, out1, out2, maxReadLen);
+		} else if (args[0].equals("dcs") && args.length == 5) {
+			// String inputBam, String sscsOutput, String dcsOutput, int maxVariance
+			String input = args[1];
+			String sscs = args[2];
+			String dcs = args[3];
+			int maxVariance = Integer.parseInt(args[4]);
+			
+			DcsProcessor dcsProcessor = new DcsProcessor();
+			dcsProcessor.dcs(input, sscs, dcs, maxVariance);
 		} else {
 			usage();
 		}
@@ -26,6 +35,8 @@ public class EfSeq {
 	}
 	
 	private static void usage() {
-		System.out.println("foo");
+		System.out.println("prep = Fastq Prep for alignment and downstream DCS.  dcs = Duplex Consensus Sequence");
+		System.out.println("java -jar efseq.jar prep <1.fastq> <2.fastq> <out1.fastq> <out2.fastq> <max_read_len>");
+		System.out.println("java -jar efseq.jar dcs <input.bam> <sscs.bam> <dcs.bam> <max_variance>");
 	}
 }
